fa42933f65a3c51fe49c864955110fb7e2ec6742
Avinash <avinash.singh@greatlearning.in>
2021-07-16 10:58:04 +0530

    Test review request

--- app/controllers/book.js
+++ app/controllers/book.js
@@ -3,6 +3,7 @@ const { Book } = require('./../models')
 
 class BookController {
   static async list (req, res) {
+    console.log("test");
     const institutionId = req.user.institution_id
 
     const books = await Book.findAll({ where: { institution_id: institutionId }, attributes: ['isbn', 'title', 'author'] })
